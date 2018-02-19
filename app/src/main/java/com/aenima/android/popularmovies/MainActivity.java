41b18dbd1806f0d1a9b7ed210050f8aee7fa0a84
xaenimax <xaenimax@users.noreply.github.com>
2018-02-19 21:52:36 +0100

    Initial setup for movie project

--- app/src/main/java/com/aenima/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/aenima/android/popularmovies/MainActivity.java
@@ -1,31 +1,40 @@
 package com.aenima.android.popularmovies;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.aenima.android.popularmovies.movieadapter.MovieAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    private Toolbar toolbar;
+
+    @BindView(R.id.movies_rv)
+    private RecyclerView movieRecyclerView;
+
+    private RecyclerView.Adapter movieAdapter;
+    RecyclerView.LayoutManager gridLayoutManager;
+    private int columnNumber = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        gridLayoutManager = new GridLayoutManager(this, columnNumber);
+        movieAdapter = new MovieAdapter();
+        movieRecyclerView.setAdapter(movieAdapter);
     }
 
     @Override
---
+++ app/src/main/java/com/aenima/android/popularmovies/movieadapter/MovieAdapter.java
@@ -0,0 +1,25 @@
+package com.aenima.android.popularmovies.movieadapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by aenim on 19/02/2018.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
