e3bb75c46ebd1dc92e873c231a6ae48c1d63d3fa
xaenimax <xaenimax@users.noreply.github.com>
2018-02-22 18:02:05 +0100

    initial setup for the recyclerview

--- app/src/main/java/com/aenima/android/popularmovies/core/movieadapter/MovieAdapter.java
+++ app/src/main/java/com/aenima/android/popularmovies/core/movieadapter/MovieAdapter.java
@@ -1,24 +1,35 @@
 package com.aenima.android.popularmovies.core.movieadapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.aenima.android.popularmovies.R;
 import com.aenima.android.popularmovies.core.model.Movie;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by aenim on 19/02/2018.
  */
 
 public class MovieAdapter extends RecyclerView.Adapter {
-    public MovieAdapter(List<Movie> movieList) {
+    List<Movie> mMovieList;
 
+
+    public MovieAdapter(List<Movie> movieList) {
+        mMovieList = movieList;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -28,6 +39,18 @@ public class MovieAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mMovieList.size();
     }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.movie_thumb_iv)
+        ImageView movieThumbImageView;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+
 }
